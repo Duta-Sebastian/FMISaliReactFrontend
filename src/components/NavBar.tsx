a479063b7d23f61d35c42e50e7e8dc8f03f24401
Duta-Sebastian <sebiduta2004@gmail.com>
2025-01-17 21:19:00 +0200

    Small adjustments to the NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Link from "next/link"
 import ThemeSwitch from "./ThemeSwitch";
 
@@ -8,18 +6,22 @@ const NavBar = () => {
         <nav className="flex items-center justify-between p-4 bg-gray-100 dark:bg-gray-800">
             <Link
                 href="https://fmi.unibuc.ro/"
-                className="text-lg font-serif font-bold text-gray-800 dark:text-gray-100"
+                className="text-lg font-serif font-bold text-gray-800 dark:text-gray-100 items-center"
             >
                 Facultatea de Matematică și Informatică
             </Link>
-            <Link
-                href="/"
-                className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700
-                 dark:bg-blue-500 dark:hover:bg-blue-600"
-            >
-                Home
-            </Link>
-            <ThemeSwitch/>
+            <div className="flex-grow flex justify-center">
+                <Link
+                    href="/"
+                    className="px-4 py-2 text-sm font-medium text-white bg-blue-600 rounded-lg hover:bg-blue-700
+                     dark:bg-blue-500 dark:hover:bg-blue-600"
+                >
+                    Home
+                </Link>
+            </div>
+            <div className="flex-shrink-0 pl-2 items-center">
+                <ThemeSwitch/>
+            </div>
         </nav>
     );
 }
